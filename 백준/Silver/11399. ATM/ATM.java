3f787dfa713862268f751181344a96df5bb52187
SKyung <87274087+imrud@users.noreply.github.com>
2023-09-02 17:18:36 +0900

    [Silver IV] Title: ATM, Time: 132 ms, Memory: 14408 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+
 public class Main{
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
@@ -20,15 +21,18 @@ public class Main{
 		Arrays.sort(arr);
 		
 		// step2. 누적합 구하기
+		int minTime = arr[0];
 		for(int i = 1; i <N; i++) {
 			arr[i] = arr[i]+arr[i-1];
+			minTime += arr[i];
+			
 		}
 		
 		// step3. 시간 합의 최솟값 출력
-		int minTime = 0;
-		for(int i = 0; i < N; i++) {
-			minTime += arr[i];
-		}
+//		int minTime = 0;
+//		for(int i = 0; i < N; i++) {
+//			minTime += arr[i];
+//		}
 		
 		System.out.println(minTime);
 
